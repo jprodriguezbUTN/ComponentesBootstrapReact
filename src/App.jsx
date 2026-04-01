4c3bc69b770965fb5ccfb703f75757efe9d430bf
Juan Pablo Rodriguez Bogantes <jprodriguezb@gmail.com>
2026-04-01 14:49:23 -0600

    inicio
    inicio

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const PHome = React.lazy(() => import('./pages/PHome.jsx'));
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<div className="container mt-4">Cargando...</div>}>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container">
+            <Link className="navbar-brand" to="/">
+              Mi Sitio
+            </Link>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<PHome />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/PHome.jsx
@@ -0,0 +1,7 @@
+export default function PHome (){
+    return(<>
+    <h1>Bienvenidos a Tecnologías y Sistemas Web I</h1>
+    <p>Veremos componetes de BootStrap adaptados a React</p>
+    <p>Ademas se segmentán en partes para una mejor optimización</p>
+    </>);
+}
