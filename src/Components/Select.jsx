56292994a0953fa8ebfa2d5ff447c5b37aaa1b0e
Felipe0220 <lusalaspe@est.utn.ac.cr>
2026-04-08 10:05:41 -0600

    Creacion componente Select

---
+++ src/Components/Select.jsx
@@ -0,0 +1,10 @@
+export default function Select( clases, arial_label, tamano, habilitado = true, opcion = false ){
+    return (
+        <select className={clases} aria-label={arial_label} size={tamano} disabled={habilitado} multiple={!opcion}>
+            <option value="">{texto}</option>
+            {opciones.map((op, index) => 
+                <option key={index} value={op.value}>{op.label}</option>  
+            )}
+        </select>
+    )    
+}
