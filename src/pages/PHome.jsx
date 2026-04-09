40a1588f74b60c1fde9b13cf8ccde5e3fb5e8ad2
Felipe0220 <lusalaspe@est.utn.ac.cr>
2026-04-09 11:27:05 -0600

    Actualizacion de eliminacion de Phome

--- src/pages/PHome.jsx
+++ src/pages/PHome.jsx
@@ -1,23 +1,5 @@
 import Popover from '../Components/PopOvers.jsx';
-import Select from "../Components/Select";
 export default function PHome() {
-  const opciones = [
-  {
-    label: "Frutas",
-    options: [
-      { value: "manzana", label: "Manzana" },
-      { value: "banana", label: "Banana" }
-    ]
-  },
-  {
-    label: "Verduras",
-    options: [
-      { value: "zanahoria", label: "Zanahoria" },
-      { value: "lechuga", label: "Lechuga" }
-    ]
-  }
-];
-
   return (
     <div className="container mt-5">
 
@@ -93,7 +75,6 @@ export default function PHome() {
           <h4 className="mb-3">
             <i className="bi bi-list-check"></i> Temas a Explorar
           </h4>
-           <Select texto="Seleccione una opción" opciones={opciones} />
           <div className="list-group">
             <div className="list-group-item d-flex justify-content-between align-items-center">
               <span>
