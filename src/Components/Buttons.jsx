04e33e4960d84188f77ab968d18ee48f562cd3c5
WENDY_MARTINEZ <wemartinezlo@est.utn.ac.cr>
2026-04-09 08:46:29 -0600

    Se modifica Buttons

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,19 +1,7 @@
-export default function Buttons() {
+export default function Buttons({ texto, color, tamano }) {
   return (
-    <div className="container mt-4 text-center">
-
-      <h3>Botones</h3>
-
-      <button type="button" class="btn btn-primary m-2">Inicio</button>
-      <button type="button" class="btn btn-success m-2">Guardar</button>
-      <button type="button" class="btn btn-danger btn-lg m-2">Eliminar</button>
-
-      <br />
-
-      <button type="button" class="btn btn-outline-primary m-2">Ver más</button>
-      <button type="button" class="btn btn-secondary m-2" disabled>Bloqueado</button>
-      <button type="button" class="btn btn-outline-dark btn-sm">Cerrar</button>
-
-    </div>
+    <button type="button" className={`btn btn-${color} ${tamano} m-2`}>
+      {texto}
+    </button>
   );
 }
