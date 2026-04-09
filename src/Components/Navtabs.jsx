8d37c848fbc5982364a021ad8ece36f855ce074b
Marco Vinicio Vasquez Barrantes <mavasquezba@est.utn.ac.cr>
2026-04-09 00:11:06 -0600

    Finalizacion del componente Navtabs
    Se agrega la personalizacion final del componente, incluyendo la capacidad de elegir si se utilizara nav o ul al encapsular los contenidos del Navtab y la capacidad de personalizar el contenido que el componente mostrara desde el codigo escrito en App.jsx

--- src/Components/Navtabs.jsx
+++ src/Components/Navtabs.jsx
@@ -1,24 +1,15 @@
 export default function Navtabs({
+    as = "ul",
     variant = "",
     alignment = "",
     extraClasses = "",
+    children
 }) {
     const Component = as;
 
     return (
-        <ul className={`nav ${variant} ${alignment} ${extraClasses}`}>
-            <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Active</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
+        <Component className={`nav ${variant} ${alignment} ${extraClasses}`}>
+            {children}
+        </Component>
     );
 }
