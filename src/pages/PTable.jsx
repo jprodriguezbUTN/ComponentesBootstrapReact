5787b8b29daea2d964b1b15b94d6fb4b82da47c2
Leandro Sanchez Rojas <lesanchezro@est.utn.ac.cr>
2026-04-09 08:16:35 -0600

    Mostrar Input en debajo de table👅

--- src/pages/PTable.jsx
+++ src/pages/PTable.jsx
@@ -1,9 +1,11 @@
 import CTable from '../components/CTable';
+import Datalist from "../Components/Datalist";
 
 export default function PTable() {
   return (
     <>
       <CTable />
+      <Datalist />
     </>
   );
 }
