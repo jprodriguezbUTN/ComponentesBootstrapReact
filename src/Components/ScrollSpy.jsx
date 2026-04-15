718efce38cce58577abd3d64e65b5784955014d1
Sebastian Villega Barquero <sebastianbarquerocr@gmail.com>
2026-04-15 15:29:12 -0600

    ScrollSpy Completo

--- src/Components/ScrollSpy.jsx
+++ src/Components/ScrollSpy.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import * as bootstrap from "bootstrap";
+
 export default function ScrollSpy({ target, children, height = "100vh" }) {
+  
+  useEffect(() => {
+    const scrollElement = document.querySelector('[data-bs-spy="scroll"]');
+
+    if (scrollElement) {
+      new bootstrap.ScrollSpy(scrollElement, {
+        target: target,
+        offset: 0
+      });
+
+    }
+  }, [target]);
   return (
     <div
       data-bs-spy="scroll"
