d5390ef62b05ff2d3e81757fb9bb8f18690f024c
Glorana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2026-04-16 01:59:25 -0600

    Se crea Pagina Carousel

---
+++ src/pages/PCarousel.jsx
@@ -0,0 +1,28 @@
+import Carousel from "../Components/Carousel";
+import Imagenes from "../Components/Images";
+
+export default function PCarousel() {
+  return (
+    <div className="container mt-4">
+
+      <h3>Carousel</h3>
+
+      <Carousel tiempo={2000}>
+
+        <div className="carousel-item">
+          <Imagenes url="img1.jpg" />
+        </div>
+
+        <div className="carousel-item">
+          <Imagenes url="img2.jpg" />
+        </div>
+
+        <div className="carousel-item">
+          <Imagenes url="img3.jpg" />
+        </div>
+
+      </Carousel>
+
+    </div>
+  );
+}
