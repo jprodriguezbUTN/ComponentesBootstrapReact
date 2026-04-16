94f11f6d960541b3212066da21a3eed041dd3fb0
Glorana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2026-04-16 08:46:21 -0600

    agregue imagenes

--- src/pages/PCarousel.jsx
+++ src/pages/PCarousel.jsx
@@ -10,15 +10,15 @@ export default function PCarousel() {
       <Carousel tiempo={2000}>
 
         <div className="carousel-item">
-          <Imagenes url="img1.jpg" />
+          <Imagenes url="https://cdn.pixabay.com/photo/2023/07/28/11/45/landscapes-8155111_1280.jpg" />
         </div>
 
         <div className="carousel-item">
-          <Imagenes url="img2.jpg" />
+          <Imagenes url="https://cdn.pixabay.com/photo/2020/03/15/13/15/desert-4933581_1280.jpg" />
         </div>
 
         <div className="carousel-item">
-          <Imagenes url="img3.jpg" />
+          <Imagenes url="https://cdn.pixabay.com/photo/2016/02/10/21/59/landscape-1192669_1280.jpg" />
         </div>
 
       </Carousel>
