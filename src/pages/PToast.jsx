249671513b740b2c45f96c548e24ba11020e786b
Sebas12212005 <Sesalashe@est.utn.ac.cr>
2026-04-16 08:22:13 -0600

    Creación de pagina de Toast

---
+++ src/pages/PToast.jsx
@@ -0,0 +1,15 @@
+import ToastSimple from "../Components/Toast";
+
+export default function ToastPage() {
+  return (
+    <div>
+      <h1>Ejemplo de Toast</h1>
+
+      <p>
+        En esta sección se muestra el uso de un Toast para notificar una acción del usuario.
+      </p>
+
+      <ToastSimple />
+    </div>
+  );
+}
